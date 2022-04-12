72f6d3c79a11fd85649ed28abba342f23f04d0e6
yueguang@qingmutec.com <yueguang@qingmutec.com>
2022-04-12 11:43:47 +0800

    签到

--- src/views/sign-in/components/sign-in-create.vue
+++ src/views/sign-in/components/sign-in-create.vue
@@ -23,24 +23,23 @@
 </template>
 
 <script>
-   export default {
-      data() {
-        return {
-          form: {
-            name: '',
-            region: '',
-            date1: '',
-          }
-        }
-      },
-      methods: {
-        onSubmit() {
-          console.log('submit!');
-        }
+export default {
+  data() {
+    return {
+      form: {
+        name: '',
+        region: '',
+        date1: ''
       }
     }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit!')
+    }
+  }
+}
 </script>
-
 
 <style lang="scss" scoped>
 @import "~@/styles/mixin.scss";
@@ -67,30 +66,37 @@
   }
 }
 
- .el-row {
-    margin-bottom: 20px;
-    &:last-child {
-      margin-bottom: 0;
-    }
+.el-row {
+  margin-bottom: 20px;
+
+  &:last-child {
+    margin-bottom: 0;
   }
-  .el-col {
-    border-radius: 4px;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
-  .bg-purple {
-    background: #d3dce6;
-  }
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-  }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
-  }
+}
+
+.el-col {
+  border-radius: 4px;
+}
+
+.bg-purple-dark {
+  background: #99a9bf;
+}
+
+.bg-purple {
+  background: #d3dce6;
+}
+
+.bg-purple-light {
+  background: #e5e9f2;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
 </style>
